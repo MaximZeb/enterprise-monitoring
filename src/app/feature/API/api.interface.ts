5c1f28e1ed65a6384053a649b91f8501782ebce0
MaximZeb <maksimzeb@yandex.ru>
2025-01-28 19:35:37 +0300

    feat(src): Создал интерфейсы

---
+++ src/app/feature/API/api.interface.ts
@@ -0,0 +1,41 @@
+export interface IResponse<T> {
+    data: T
+}
+
+export interface ILoginEntery {
+    login: string;
+    password: string;
+} 
+
+export interface ILoginEntery {
+    login: string;
+    password: string;
+} 
+
+export interface ILoginCreate {
+    login: string;
+    password: string;
+    name: string;
+    surname: string;
+    middleName: string;
+    position: string;
+    division: string;
+}
+
+export interface IMine {
+    _id: string;
+    name: string;
+    sections: ISection[];
+}
+
+export interface ISection {
+    name: string;
+    combine_complexs: ICombineComplexs[];
+}
+
+export interface ICombineComplexs {
+    name: string;
+    combineId: string;
+    bunkerId: string;
+    samohodniVagonId: string;
+}
--- src/app/feature/API/api.service.ts
+++ src/app/feature/API/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -27,6 +27,9 @@ export class ApiService {
     return this.http.get(url, {
       headers: headers,
       withCredentials: true
-    });
+    }).pipe(
+      map((answer: any) => {
+        return answer.data
+    }));
   }
 }
