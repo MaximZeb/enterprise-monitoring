9d11709ee0b3c52210d96b18582aa965edf8ad8f
MaximZeb <maksimzeb@yandex.ru>
2025-02-13 19:43:36 +0300

    monitoring-0003-technics feat(MineTechnincsHeadComponent): Сделал часть верстки добавил chart js

--- src/app/feature/MonitoringModule/mine-technincs-head/mine-technincs-head.component.ts
+++ src/app/feature/MonitoringModule/mine-technincs-head/mine-technincs-head.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-mine-technincs-head',
   templateUrl: './mine-technincs-head.component.html',
   styleUrls: ['./mine-technincs-head.component.scss']
 })
-export class MineTechnincsHeadComponent implements OnInit {
+export class MineTechnincsHeadComponent implements OnInit, AfterViewInit  {
+  @ViewChild('myChart') myChart!: ElementRef<HTMLCanvasElement>;
+
   public technicsForm: FormGroup = new FormGroup({
       date: new FormControl('', Validators.required),
       workingShift: new FormControl('', Validators.required)
@@ -17,9 +21,34 @@ export class MineTechnincsHeadComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public ngAfterViewInit(): void {
+    const ctx = this.myChart.nativeElement.getContext('2d');
+
+    const myChart = new Chart(ctx as CanvasRenderingContext2D, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], // время
+        datasets: [{
+          label: 'Проходка, м',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+          backgroundColor: "#07c184"
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
   public findTechincs(): void {
     if (this.technicsForm.valid) {
       console.log(this.technicsForm.value)
     }
   }
 }
+
